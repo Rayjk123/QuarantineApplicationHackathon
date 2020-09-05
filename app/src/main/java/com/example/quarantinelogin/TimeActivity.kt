e7b84b363a1e6e4d1ce9396823e02c7573a4e349
heeyek <heeyek@gmail.com>
2020-09-05 14:55:44 -0400

    Admin stuff

--- app/src/main/java/com/example/quarantinelogin/TimeActivity.kt
+++ app/src/main/java/com/example/quarantinelogin/TimeActivity.kt
@@ -2,10 +2,33 @@ package com.example.quarantinelogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.quarantinelogin.fragments.MapFragment
+import com.example.quarantinelogin.fragments.TimeFragment
+import kotlinx.android.synthetic.main.activity_time.*
 
 class TimeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time)
+
+        val timeFragment = TimeFragment()
+        val mapFragment = MapFragment()
+
+        makeCurrentFragment(timeFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.ic_time -> makeCurrentFragment(timeFragment)
+                R.id.ic_map -> makeCurrentFragment(mapFragment)
+            }
+            true
+        }
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
 }
