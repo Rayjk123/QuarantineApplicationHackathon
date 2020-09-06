57dc1d9f370e5748cf5b927ffa1c4c187e0c9874
Panda <bdleecs95@gmail.com>
2020-09-06 00:40:38 -0400

    Moving logic to fragement

--- app/src/main/java/com/example/quarantinelogin/TimeActivity.kt
+++ app/src/main/java/com/example/quarantinelogin/TimeActivity.kt
@@ -1,21 +1,13 @@
 package com.example.quarantinelogin
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.quarantinelogin.fragments.MapFragment
 import com.example.quarantinelogin.fragments.TimeFragment
 import kotlinx.android.synthetic.main.activity_time.*
-import okhttp3.*
-import okhttp3.MediaType.Companion.toMediaType
-import org.json.JSONException
-import java.io.IOException
 
 class TimeActivity : AppCompatActivity() {
-    val JSON: MediaType = "application/json; charset=utf-8".toMediaType()
-    var client = OkHttpClient()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time)
@@ -32,55 +24,6 @@ class TimeActivity : AppCompatActivity() {
             }
             true
         }
-
-        val phoneNumber = LoginActivity.Companion.user?.getJSONObject("Item")?.getJSONObject("phoneNumber")?.getString("S")
-        getQuarantineEndTime(phoneNumber, object: Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                println("Request Failure.")
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                val body = response.body?.string()
-                println(body)
-                if (body != null) {
-                    val QuarantineEndTimeInMilliseconds = body.toLong()
-
-                    runOnUiThread {
-                        try {
-                            // TODO Get Time Left = QuarantineEndTimeInMilliseconds - CURRENT TIME IN MILLISECONDS
-
-                            // TODO DISPLAY TIME LEFT IN TEXT FORMAT ON SCREEN
-
-                            println("Request Successful to Get Quarantine Time!")
-                            println(QuarantineEndTimeInMilliseconds)
-                            Toast.makeText(
-                                this@TimeActivity,
-                                "Created Geofence Successfully",
-                                Toast.LENGTH_SHORT
-                            ).show()
-
-                        } catch (e: JSONException) {
-                            Toast.makeText(
-                                this@TimeActivity,
-                                "An Error occurred while creating the Geofence. Please try again.",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            e.printStackTrace()
-                        }
-                    }
-                }
-            }
-        });
-    }
-
-    @Throws(IOException::class)
-    fun getQuarantineEndTime(phoneNumber: String?, callback: Callback): Unit {
-        val request = Request.Builder()
-            .url("https://e2d600v4b3.execute-api.us-east-1.amazonaws.com/dev/user/$phoneNumber/quarantineTimer")
-            .get()
-            .build()
-
-        return client.newCall(request).enqueue(callback)
     }
 
     private fun makeCurrentFragment(fragment: Fragment) =
--- app/src/main/java/com/example/quarantinelogin/fragments/TimeFragment.kt
+++ app/src/main/java/com/example/quarantinelogin/fragments/TimeFragment.kt
@@ -1,11 +1,17 @@
 package com.example.quarantinelogin.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.quarantinelogin.LoginActivity
 import com.example.quarantinelogin.R
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import org.json.JSONException
+import java.io.IOException
 
 
 /**
@@ -14,14 +20,64 @@ import com.example.quarantinelogin.R
  * create an instance of this fragment.
  */
 class TimeFragment : Fragment() {
+    val JSON: MediaType = "application/json; charset=utf-8".toMediaType()
+    var client = OkHttpClient()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val phoneNumber = LoginActivity.Companion.user?.getJSONObject("Item")?.getJSONObject("phoneNumber")?.getString("S")
+        getQuarantineEndTime(phoneNumber, object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Request Failure.")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                println(body)
+                if (body != null) {
+                    val QuarantineEndTimeInMilliseconds = body.toLong()
+
+                    activity?.runOnUiThread {
+                        try {
+                            // TODO Get Time Left = QuarantineEndTimeInMilliseconds - CURRENT TIME IN MILLISECONDS
+
+                            // TODO DISPLAY TIME LEFT IN TEXT FORMAT ON SCREEN
+
+                            println("Request Successful to Get Quarantine Time!")
+                            println(QuarantineEndTimeInMilliseconds)
+                            Toast.makeText(
+                                activity,
+                                "Created Geofence Successfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                        } catch (e: JSONException) {
+                            Toast.makeText(
+                                activity,
+                                "An Error occurred while creating the Geofence. Please try again.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            e.printStackTrace()
+                        }
+                    }
+                }
+            }
+        })
         return inflater.inflate(R.layout.fragment_time, container, false)
     }
 
 
+    @Throws(IOException::class)
+    fun getQuarantineEndTime(phoneNumber: String?, callback: Callback): Unit {
+        val request = Request.Builder()
+            .url("https://e2d600v4b3.execute-api.us-east-1.amazonaws.com/dev/user/$phoneNumber/quarantineTimer")
+            .get()
+            .build()
+
+        return client.newCall(request).enqueue(callback)
+    }
 }
